3040bbed38a9f81ddfc240ad58703e0cecd71755
Justin Hurst <jryanhurst@gmail.com>
2019-02-01 14:02:25 -0700

    Solve the shout case

--- lib/greetings.ex
+++ lib/greetings.ex
@@ -1,5 +1,13 @@
 defmodule Greetings do
 
-  def greet(name), do: "Hello, #{name}."
+  def greet(name) do
+    case String.upcase(name) == name do
+      true -> shout(name)
+      false -> gentle(name)
+    end
+  end
   def greet(), do: greet("my friend")
+  def gentle(name), do: "Hello, #{name}."
+  def shout(name), do: "HELLO #{name}!"
+  
 end
