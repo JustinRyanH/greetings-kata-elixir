a7c95ca8552690c6444df2bbf81a3de64fe8ab82
Jason Carter <jason@mavenlink.com>
2019-02-01 09:43:13 -0700

    Reset to baseline

--- lib/greetings.ex
+++ lib/greetings.ex
@@ -1,55 +1,6 @@
 defmodule Greetings do
 
   def greet() do
-    greet("my friend")
+    # implement me
   end
-
-  def greet([name | more_names]) do
-    [name | more_names] |> 
-    Enum.partition(&shout?(&1)) |>
-    greet()
-  end
-
-  def greet({[], salutes}) do
-    salutes |> salute
-  end
-
-  def greet({shouts, []}) do
-    shouts |> shout
-  end
-
-  def greet({shouts, salutes}) do
-    salute(salutes) <> " AND " <> shout(shouts)
-  end
-
-  def greet(name) do
-    cond do
-      name |> shout?() -> name |> shout
-      true -> name |> salute
-    end
-  end
-
-  defp salute([name | tail]) do
-    salute(name, tail)
-  end
-
-  defp salute(name) do
-    "Hello, #{name}."
-  end
-
-  defp salute(accumulated, [name | []]) do
-    "#{accumulated} and #{name}" |> greet
-  end  
-
-  defp salute(accumulated, [name | more_names]) do
-    "#{accumulated}, #{name}," |> salute(more_names)    
-  end
-
-  defp shout(name) do
-    "HELLO #{name}!"
-  end
-
-  defp shout?(name) do
-    String.upcase(name) == name
-  end 
 end
--- test/greetings_test.exs
+++ test/greetings_test.exs
@@ -4,26 +4,5 @@ defmodule GreetingsTest do
 
   test "greets by name" do
     assert Greetings.greet("Bob") == "Hello, Bob."
-    assert Greetings.greet("Jill") == "Hello, Jill."
-  end
-
-  test "handles null" do
-    assert Greetings.greet() == "Hello, my friend."
-  end
-
-  test "shouts back" do
-    assert Greetings.greet("JERRY") == "HELLO JERRY!"
-  end
-
-  test "handles two names" do
-    assert Greetings.greet(["Jill", "Jane"]) ==  "Hello, Jill and Jane."
-  end
-
-  test "handles arbitrary number of names" do
-    assert Greetings.greet(["Amy", "Brian", "Charlotte"]) == "Hello, Amy, Brian, and Charlotte."
-  end
-
-  test "allows mixing of normal and shouted names by separating the response into two greetings" do
-    assert Greetings.greet(["Amy", "BRIAN", "Charlotte"]) ==  "Hello, Amy and Charlotte. AND HELLO BRIAN!"
   end
 end
